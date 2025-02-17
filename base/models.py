3fff6c1a4cd172acf2d647ad0fce090b6dc6534c
Buchi77344 <onyebuchifestus77@gmail.com>
2025-02-17 08:04:28 +0100

    xsdjnhsuh

--- base/models.py
+++ base/models.py
@@ -158,3 +158,18 @@ class Ask(models.Model):
     def __str__(self):
         return self.add_title
 
+class Payment(models.Model):
+    STATUS_CHOICES = (
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+        ('Failed', 'Failed')
+    )
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=50)
+    transaction_id = models.CharField(max_length=100, unique=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.status}"
